d79c41bc8d75dc5d457c9eaa2b820d6fed74a7e6
Pablo Pérez Martín <aperezvic21@gmail.com>
2023-05-13 12:49:48 +0200

    Primer commit

---
+++ entrega.sql
@@ -0,0 +1,9 @@
+
+
+-- 1.Vistas de Ocupación
+/*
+1. Crear una vista V_OCUPACION_ASIGNADA que reúna las tablas de sedes, examen y
+asistencia y devuelva el código de sede, su nombre, código de aula, fecha de examen y
+número de estudiantes asignados a un examen, agrupando por dicha sede, su nombre,
+código de aula y fecha de examen.
+*/
